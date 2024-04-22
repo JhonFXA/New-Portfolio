e6ea918ac6256b0a664e8436c48746fb6507ca93
Jonathan Henrique <jonathanbispo085@gmail.com>
2024-04-22 20:45:55 -0300

    adiciona seção de início

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,30 @@
+const header = document.getElementById("header");
+
 function toggleMenu(){
     var menuBtn = document.getElementById("nav-menu");
 
     if(menuBtn.className === "nav-menu"){
         menuBtn.className += " responsive";
+        header.style.backgroundColor = "var(--primary-color)";
     } else {
         menuBtn.className = "nav-menu";
+        header.style.backgroundColor = "var(--white-color)";
     }
 }
+
+function headerShadow(){
+    var scrollBtn = document.getElementById("scroll-btn");
+    if(document.documentElement.scrollTop > 50){
+        scrollBtn.style.display = "none";
+        header.style.boxShadow = "0 1px 6px rgba(0, 0, 0 , 0.1)";
+        header.style.height = "70px";
+    }else{
+        scrollBtn.style.display = "flex";
+        header.style.boxShadow = "none";
+        header.style.height = "90px";
+    }
+}
+
+window.onscroll = function(){
+    headerShadow();
+};
